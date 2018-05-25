15b5794540006f0c9794e6692a6562d1f7ce0012
Pabi <pmoloi@jhb.dvt.co.za>
2018-05-25 14:42:33 +0200

    Login functionality.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
+import {ReactiveFormsModule, FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -12,7 +13,9 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
+import {FormBuilder, FormGroup, Validators} from '@angular/forms'
+import { reject } from 'q';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +10,27 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
+
+  loginForm: FormGroup;
+  filledIn = false;
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      loginEmail:['',[Validators.required, Validators.email]],
+      loginPassword:['',Validators.required]
+    });
   }
-
+  get controller()
+  {
+    return this.loginForm.controls;
+  }
+  onLoginButtonClick(){
+    console.log("function called");
+    console.log(this.loginForm.value);
+    if (this.loginForm.invalid)
+    {
+      return;
+    }
+  }
 }
